b185b52c8226c22cd66dceefc7af1c1448df8748
monkeshrek <d303802@edu.curio.nl>
2024-12-11 16:07:09 +0100

    bankapp

---
+++ bankSimulator/bankSimulator/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class BankSimulator
+{
+    internal class bankAccount
+    {
+        decimal totaalSaldo;
+        public void Storten()
+        {
+            do 
+            {
+                Console.WriteLine("hoeveel wil je storten");
+                if (decimal.TryParse(Console.ReadLine(), out decimal storten))
+                {
+                    totaalSaldo += storten;
+                    Console.WriteLine($"je hebt {storten} gestort, je hebt nu {totaalSaldo} euro");
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("ongeldige invoer");
+                }
+            } while (true);
+        }
+        public void Opnemen() 
+        {
+            do
+            {
+                Console.WriteLine("hoeveel geld wil je opnemen?");
+                if (decimal.TryParse(Console.ReadLine(), out decimal opnemen))
+                {
+                    if (totaalSaldo < opnemen)
+                    {
+                        Console.WriteLine("te weinig saldo");
+                    }
+                    else 
+                    {
+                        totaalSaldo -= opnemen;
+                        Console.WriteLine($"je hebt {opnemen} opgenomen, je totaalsaldo is nu {totaalSaldo} euro"); 
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("ongeldige invoer");
+                }
+            } while (true);   
+        }
+        public void Saldo() 
+        {
+            {
+                Console.WriteLine($"je saldo is {totaalSaldo} euro");
+            }
+        }
+    }
+}
---
+++ bankSimulator/bankSimulator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Xml;
+using static BankSimulator;
+
+namespace bankSimulator
+{
+    class bankSimulator
+    {
+        internal class BankSimulator
+        {
+            static void Main(string[] args)
+            {
+                bankAccount account = new bankAccount();
+
+
+                while (true)
+                {
+                    Console.WriteLine("welkom bij de bank. maak uw keuze");
+                    Console.WriteLine("1. geld storten");
+                    Console.WriteLine("2. geld opnemen");
+                    Console.WriteLine("3. saldo bekijken");
+                    Console.WriteLine("4. exit");
+                    Console.WriteLine("maak een keuze");
+
+                    string keuze = Console.ReadLine();
+
+                    if (keuze == "1")
+                    {
+                        account.Storten();
+                    }
+                    if (keuze == "2")
+                    {
+                        account.Opnemen();
+                    }
+                    if (keuze == "3")
+                    {
+                       account.Saldo();
+                    }
+                    if(keuze == "4") 
+                    {
+                        break;
+                    }
+
+
+
+
+                }
+            }
+
+        }
+    }
+}
